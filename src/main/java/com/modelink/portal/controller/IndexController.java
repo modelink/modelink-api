533546ab49568b8b453ea83c82f0026c97aca419
dyubo2005 <dyubo2005@163.com>
2018-07-25 21:16:44 +0800

    add index page

--- src/main/java/com/modelink/portal/controller/IndexController.java
+++ src/main/java/com/modelink/portal/controller/IndexController.java
@@ -2,13 +2,29 @@ package com.modelink.portal.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping
 public class IndexController {
 
     @RequestMapping(value = {"", "/", "index"})
-    public String index() {
-        return "index";
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("/index/index");
+        return modelAndView;
     }
+
+    @RequestMapping("/introduce")
+    public ModelAndView introduce(@RequestParam String index) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("/index/introduce");
+        modelAndView.addObject("index", index);
+        return modelAndView;
+    }
+
+
 }
